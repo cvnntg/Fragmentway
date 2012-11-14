668317ff208b70f6b1d57c79b28e0ae4af9afb3d
Uhnyak Goo <cvnntg@gmail.com>
2012-11-14 17:46:26 -0500

    First Upload

---
+++ src/com/example/fragmentway/Tab1Fragment.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.example.fragmentway;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+/**
+ * @author mwho
+ *
+ */
+public class Tab1Fragment extends Fragment implements SensorEventListener{
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (container == null) {
+            // We have different layouts, and in one of them this
+            // fragment's containing frame doesn't exist.  The fragment
+            // may still be created from its saved state, but there is
+            // no reason to try to create its view hierarchy because it
+            // won't be displayed.  Note this is not needed -- we could
+            // just run the code below, where we would create and return
+            // the view hierarchy; it would just never be used.
+            return null;
+        }
+		return (LinearLayout)inflater.inflate(R.layout.tab_frag1_layout, container, false);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
